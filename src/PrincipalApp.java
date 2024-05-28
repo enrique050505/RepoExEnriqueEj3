e548475be8a91f369eaacedcd8c8d4ac50c53490
enrique050505 <enriquemartinalbodo@gmail.com>
2024-05-28 10:50:22 +0200

    añado la funcionalidad a la clase main

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
 public class PrincipalApp {
+	
+	public static void main (String [] args) {
+		ArrayList<Integer> edades = new ArrayList<Integer>();
+		int edad;
+		String frase;
+		
+		
+		for(int i=0; i<10; i++) {
+			edad=(int)(1+Math.random()*100);
+			edades.add(edad);
+		}
+		
+		AnalisisEdad analizarEdades = new AnalisisEdad (edades);
+		
+		frase = analizarEdades.devolverFrase(edades);
+		System.out.println(frase);
+	}//FIN MAIN
 
-}
+}//FIN CLASS
